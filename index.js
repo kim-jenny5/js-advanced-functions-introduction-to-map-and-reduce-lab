03c57d74ce6133dbfbad510fd1c3ae71a8c9abca
Jenny Kim <kimj1905@gmail.com>
2021-09-14 13:15:33 -0400

    commit tree 3d14f7afc5a947158478fa53afddf8c2665ad3ae

--- index.js
+++ index.js
@@ -1,1 +1,55 @@
-// Your code here
+function mapToNegativize(arr) {
+  const newArr = [];
+  for (const num of arr) {
+    newArr.push(-num);
+  }
+  return newArr;
+}
+
+function mapToNoChange(arr) {
+  const newArr = [];
+  for (const num of arr) {
+    newArr.push(num);
+  }
+  return newArr;
+}
+
+function mapToDouble(arr) {
+  const newArr = [];
+  for (const num of arr) {
+    newArr.push(num * 2);
+  }
+  return newArr;
+}
+
+function mapToSquare(arr) {
+  const newArr = [];
+  for (const num of arr) {
+    newArr.push(num ** 2);
+  }
+  return newArr;
+}
+
+function reduceToTotal(arr, x = 0) {
+  let aggregate = x;
+  for (const num of arr) {
+    aggregate += num;
+  }
+  return aggregate;
+}
+
+function reduceToAllTrue(arr) {
+  let truthy;
+  for (const num of arr) {
+    !!num === true ? (truthy = true) : (truthy = false);
+  }
+  return truthy;
+}
+
+function reduceToAnyTrue(arr) {
+  let truthy;
+  for (const num of arr) {
+    !!num === true ? (truthy = true) : (truthy = false);
+  }
+  return truthy;
+}
